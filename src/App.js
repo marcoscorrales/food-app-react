21278805ef7c7969ddacd7f02d91b4c12802ac60
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-10-07 08:42:31 +0200

    Añado componentes hero, headline ,food y el data con todas las imagenes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Food from "./components/Food";
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
     </div>
   );
 }
